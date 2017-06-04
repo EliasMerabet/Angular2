341994616f191cb97f1b437e32aebaa39562a502
Elias Merabet Ponton <elias.merabet.p@gmail.com>
2017-06-04 16:45:49 +0200

    Create pelicula-data.ts

---
+++ Angular 2.4/HelloWorldFinal/app/pelicula-data.ts
@@ -0,0 +1,9 @@
+import {Pelicula} from './pelicula.model';
+
+export const CARTELERA: Pelicula[] = [
+	{id:1, titulo: 'Matrix', genero: 'SCI-FI', duracion: 125},
+	{id:2, titulo: 'En Busca de la Felicidad', genero: 'Thriller', duracion: 100},
+	{id:3, titulo: 'Aladdin', genero: 'Dibujos', duracion: 90},
+	{id:4, titulo: 'El Caballero Oscuro', genero: 'SCI-FI', duracion: 105},
+	{id:5, titulo: 'Cadena Perpetua', genero: 'Drama', duracion: 120}
+];
