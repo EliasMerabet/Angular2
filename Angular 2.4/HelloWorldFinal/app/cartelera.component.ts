ed278375a0a472a7031bfa0a594414df2e308771
Elias Merabet Ponton <elias.merabet.p@gmail.com>
2017-06-04 16:41:43 +0200

    Create cartelera.component.ts

---
+++ Angular 2.4/HelloWorldFinal/app/cartelera.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import {CarteleraListComponent} from './cartelera-list.component'
+ 
+@Component({
+  selector: 'cartelera',
+  templateUrl: '../cartelera.html',
+  providers: [CarteleraListComponent],
+})
+
+export class CarteleraComponent {
+}
